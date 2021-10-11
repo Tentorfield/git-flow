5d151adf84dc36557908f4da288e6b94ff103c20
Tentorfield <hngo017@ucr.edu>
2021-10-11 14:00:13 -0700

    linking wordCount.h to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include "wordCount.h"
 #include <string>
+
+int SIZE = 100;
 
 int main()
 {
-    std::string name;
-    std::cout << "What is your name?" << std::endl;
-    std::cin >> name;
-    std::cout << "Hello " << name << "!" << std::endl;
-
+    string name;
+    char input[SIZE];
+    cout << "What is your name?" << endl;
+    cin >> name;
+    cin.getline(input, SIZE);
+    cout << "Hello " << name << "!" << endl;
+    cout << "Word count: " << wordCount(input);
     return 0;
 }
 
