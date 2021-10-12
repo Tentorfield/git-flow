813f87fced570e6225be9fe9566de9163a21166b
Harry64C <hcoop006@ucr.edu>
2021-10-11 23:29:34 -0700

    added main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
-#include "wordCount.h"
-#include <string>
+#include "c-echo.h"
 
-int SIZE = 100;
-
-int main()
-{
-    string name;
-    char input[SIZE];
-    cout << "What is your name?" << endl;
-    cin >> name;
-    cin.getline(input, SIZE);
-    cout << "Hello " << name << "!" << endl;
-    cout << "Word count: " << wordCount(input);
-    return 0;
+int main(int argv, char** argc) {
+    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
 }
+
 
