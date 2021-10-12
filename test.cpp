193821fd5f82624028470927804beab8b35f7ea9
Harry64C <hcoop006@ucr.edu>
2021-10-11 17:24:41 -0700

    implemented test.cpp tests

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 #include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
 
 TEST(EchoTest, HelloWorld) {
     char* test_val[3]; 
-    test_val[0] = "./c-echo"; 
+    test_val[0] = "./c-echo-count"; 
     test_val[1] = "hello"; 
     test_val[2] = "world";
     EXPECT_EQ("hello world", echo(3,test_val));
@@ -12,26 +13,30 @@ TEST(EchoTest, HelloWorld) {
 
 TEST(EchoTest, EmptyString) {
     char* test_val[1]; 
-    test_val[0] = "./c-echo";
+    test_val[0] = "./c-echo-count";
     EXPECT_EQ("", echo(1,test_val));
 }
 
-
-TEST(EchoTest, LongString) {
-    char* test_val[7]; 
-    test_val[0] = "./c-echo";
-    test_val[1] = "Hello";
-    test_val[2] = "world,";
-    test_val[3] = "once";
-    test_val[4] = "again";
-    test_val[5] = "we";
-    test_val[6] = "meet.";
-    EXPECT_EQ("Hello world, once again we meet.", echo(7,test_val));
+//the following 3 tests are copied from the instructions
+TEST(CountTest, HelloWorld) {
+    std::string test_str = "hello world";
+    EXPECT_EQ(2, count(test_str));
 }
 
+TEST(CountTest, EmptyString) {
+    std::string test_str = "";
+    EXPECT_EQ(0, count(test_str));
+}
+
+TEST(CountTest, ManySpaces) {
+    std::string test_str = "   this   string has     weird   spacing";
+    EXPECT_EQ(5, count(test_str));
+}
+
+// these 2 tests are from Harrioson's lab 1
 TEST(EchoTest, SpecialChars) {
     char* test_val[6];
-    test_val[0] = "./c-echo";
+    test_val[0] = "./c-echo-count";
     test_val[1] = "!";
     test_val[2] = "@";
     test_val[3] = "#";
@@ -42,11 +47,12 @@ TEST(EchoTest, SpecialChars) {
 
 TEST(EchoTest, AlphaNumericStrings) {
     char* test_val[3];
-    test_val[0] = "./c-echo";
+    test_val[0] = "./c-echo-count";
     test_val[1] = "F1gur471v3ly";
     test_val[2] = "5p34k1ng";
     EXPECT_EQ("F1gur471v3ly 5p34k1ng", echo(3,test_val));
 }
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
